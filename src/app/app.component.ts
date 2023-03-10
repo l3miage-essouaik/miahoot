e58cc3d07557fa6f58a269da75cbefde0097e095
l3miage-essouaik <hirohito595@gmail.com>
2023-03-10 12:06:43 +0100

    profile picture

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,96 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnDestroy, OnInit, Optional, ViewChild } from '@angular/core';
+import { EMPTY, Observable, Subscription } from 'rxjs';
+import { Auth, authState,onAuthStateChanged, signInAnonymously, signOut, User, GoogleAuthProvider, signInWithPopup, } from '@angular/fire/auth';
+import { traceUntilFirst } from '@angular/fire/performance';
+import { map } from 'rxjs/operators';
+import { BehaviorSubject } from 'rxjs';
+import { MatProgressSpinner } from '@angular/material/progress-spinner';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit, OnDestroy {
+
+  @ViewChild(MatProgressSpinner) spinner: MatProgressSpinner | undefined;
   title = 'miahoot';
+
+  public isLoading = new BehaviorSubject<boolean>(false);
+
+  private readonly userDisposable: Subscription|undefined;
+  public readonly user: Observable<User | null> = EMPTY;
+  public user$: Observable<User | null>; // an observable that emits the currently logged in user or null
+  public userPhotoUrl: string; 
+
+  showLoginButton = false;
+  showLogoutButton = false;
+  //optional : pour le cas ou on ne puisse pas faire l'injection 
+  constructor(@Optional() private auth: Auth) {
+    if (auth) {
+      this.user = authState(this.auth);
+      this.userDisposable = authState(this.auth).pipe(
+        traceUntilFirst('auth'),
+        map(u => !!u)
+      ).subscribe(isLoggedIn => {
+        this.showLoginButton = !isLoggedIn;
+        this.showLogoutButton = isLoggedIn;
+      });
+    }
+  }
+
+  showDropdown = false;
+
+  toggleDropdown() {
+    this.showDropdown = !this.showDropdown;
+  }
+
+  ngOnDestroy(): void {
+    if(this.userDisposable) {
+      this.userDisposable.unsubscribe();
+    }
+    
+  }
+  
+  ngOnInit() {
+    onAuthStateChanged(this.auth, user => {
+      if (user) {
+        this.userPhotoUrl = user.photoURL ?? ''; // si c'est null on retourne ""
+      } else {
+        this.userPhotoUrl = ''; // set to empty string if no user is logged in
+      }
+    });
+  }
+
+  async login() {
+    try {
+      this.isLoading.next(true);
+      await signInWithPopup(this.auth, new GoogleAuthProvider());
+    } catch (error) {
+      console.error('Error signing in with Google', error);
+    } finally {
+      this.isLoading.next(false);
+    }
+  }
+  
+
+  async loginAnonymously() {
+    try {
+      this.isLoading.next(true);
+      await signInAnonymously(this.auth);
+    } catch (error) {
+      console.error('Error signing in with Google', error);
+    } finally {
+      this.isLoading.next(false);
+    }
+  }
+
+  async logout() {
+    return await signOut(this.auth);
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,19 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
 import { environment } from '../environments/environment';
-import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideAuth,getAuth, } from '@angular/fire/auth';
 import { provideFirestore,getFirestore } from '@angular/fire/firestore';
 import { AccueilComponent } from './accueil/accueil.component';
 import { AccountConfigComponent } from './account-config/account-config.component';
 import {MatToolbarModule} from '@angular/material/toolbar'; 
+import { MatIconModule } from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button'; 
+import {MatMenuModule} from '@angular/material/menu'; 
+import {MatInputModule} from '@angular/material/input'; 
+import {MatFormFieldModule} from '@angular/material/form-field'; 
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner'; 
+import {MatCheckboxModule} from '@angular/material/checkbox'; 
+import {MatRadioModule} from '@angular/material/radio'; 
 
 @NgModule({
   declarations: [
@@ -22,6 +30,14 @@ import {MatToolbarModule} from '@angular/material/toolbar';
     BrowserModule,
     MatToolbarModule,
     AppRoutingModule,
+    MatIconModule,
+    MatButtonModule,
+    MatMenuModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatProgressSpinnerModule,
+    MatCheckboxModule,
+    MatRadioModule,
     BrowserAnimationsModule,
     provideFirebaseApp(() => initializeApp(environment.firebase)),
     provideAuth(() => getAuth()),
